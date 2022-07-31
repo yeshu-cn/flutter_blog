53f96330d303946a2a19733ceeae86b19267af68
yeshu <guan.yeshu@gmail.com>
2022-07-31 23:17:48 +0800

    markdown 渲染的效果不好，而且感觉点击选中字符的时候感觉卡

--- lib/ui/about.dart
+++ lib/ui/about.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_blog/ui/top_bar.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:markdown_widget/markdown_widget.dart';
 
 class About extends StatefulWidget {
   const About({Key? key}) : super(key: key);
@@ -34,29 +35,33 @@ class _AboutState extends State<About> {
             ),
             Padding(
               padding: const EdgeInsets.only(left: 500, right: 500, top: 40),
-              child: Row(
-                children: [
-                  Text(
-                    '兴趣：听歌写代码，爱好足球，滑雪',
-                    style: theme.textTheme.bodyText1,
-                  ),
-                ],
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.only(left: 500, right: 500, top: 10),
-              child: Row(
-                children: [
-                  Text(
-                    '邮箱：guan.yeshu@gmail.com',
-                    style: theme.textTheme.bodyText1,
-                  ),
-                ],
+              child: FutureBuilder(
+                builder:
+                    (BuildContext context, AsyncSnapshot<String> snapshot) {
+                  if (snapshot.hasData) {
+                    return MarkdownWidget(
+                      styleConfig: StyleConfig(
+
+                      ),
+                      data: snapshot.data!,
+                      shrinkWrap: true,
+                    );
+                  } else {
+                    return const Center(
+                      child: CircularProgressIndicator(),
+                    );
+                  }
+                },
+                future: _loadAboutMarkdown(),
               ),
             ),
           ],
         ),
       ),
     );
+  }
+
+  Future<String> _loadAboutMarkdown() async {
+    return await rootBundle.loadString('assets/pages/test.md');
   }
 }
--- lib/ui/post_detail_page.dart
+++ lib/ui/post_detail_page.dart
@@ -1,8 +1,12 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
 import 'package:flutter_blog/di/di.dart';
 import 'package:flutter_blog/domain/post_service.dart';
+import 'package:flutter_blog/ui/toc_item.dart';
 import 'package:flutter_blog/ui/top_bar.dart';
-import 'package:flutter_markdown/flutter_markdown.dart';
+import 'package:markdown_widget/config/widget_config.dart';
+import 'package:markdown_widget/markdown_widget.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class PostDetailPage extends StatefulWidget {
   final int createTime;
@@ -14,6 +18,8 @@ class PostDetailPage extends StatefulWidget {
 }
 
 class _PostDetailPageState extends State<PostDetailPage> {
+  final TocController _tocController = TocController();
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -21,27 +27,123 @@ class _PostDetailPageState extends State<PostDetailPage> {
         child: Column(
           children: [
             const TopBar(),
-            Padding(
-              padding: const EdgeInsets.only(left: 500, right: 500),
-              child: FutureBuilder(
-                future: getIt.get<PostService>().getPostDetail(widget.createTime),
-                builder: (BuildContext context, AsyncSnapshot<PostDetail> snapshot) {
-                  if (snapshot.hasData) {
-                    return Markdown(
-                      data: snapshot.data!.content,
-                      shrinkWrap: true,
-                    );
-                  } else {
-                    return const Center(
-                      child: CircularProgressIndicator(),
-                    );
-                  }
-                },
-              ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: [
+                SizedBox(
+                  width: 1000,
+                  child: FutureBuilder(
+                    future: getIt
+                        .get<PostService>()
+                        .getPostDetail(widget.createTime),
+                    builder: (BuildContext context,
+                        AsyncSnapshot<PostDetail> snapshot) {
+                      if (snapshot.hasData) {
+                        return _buildMarkdown(snapshot.data!.content);
+                      } else {
+                        return const Center(
+                          child: CircularProgressIndicator(),
+                        );
+                      }
+                    },
+                  ),
+                ),
+              ],
             ),
           ],
         ),
       ),
     );
+  }
+
+  Widget _buildTocWidget() {
+    return TocListWidget(
+      controller: _tocController,
+      tocItem: (toc, isCurrent) {
+        return TocItemWidget(
+          isCurrent: isCurrent,
+          toc: toc,
+          onTap: () {
+            _tocController.jumpTo(index: toc.index);
+          },
+        );
+      },
+    );
+  }
+
+  Widget _buildMarkdown(String data) => MarkdownWidget(
+        data: data,
+        // childMargin: const EdgeInsets.only(top: 20),
+        shrinkWrap: true,
+        // widgetConfig: WidgetConfig(p: (node) {
+        //   return PWidget(
+        //     children: node.children,
+        //     parentNode: node,
+        //     textConfig: TextConfig(textAlign: TextAlign.start),
+        //   );
+        // }),
+        styleConfig: StyleConfig(
+          markdownTheme: MarkdownTheme.lightTheme,
+          pConfig: PConfig(
+            textStyle: const TextStyle(height: 1.5),
+            onLinkTap: (url) {
+              if (null != url) {
+                launchUrl(Uri.parse(url));
+              }
+            },
+          ),
+          preConfig: PreConfig(
+            preWrapper: (child, text) {
+              return Stack(
+                children: <Widget>[
+                  child,
+                  Container(
+                    margin: const EdgeInsets.only(top: 5, right: 5),
+                    alignment: Alignment.topRight,
+                    child: IconButton(
+                      onPressed: () {
+                        Clipboard.setData(ClipboardData(text: text));
+                        ScaffoldMessenger.of(context).showSnackBar(
+                            const SnackBar(content: Text('复制成功')));
+                      },
+                      icon: const Icon(
+                        Icons.content_copy,
+                        size: 10,
+                      ),
+                    ),
+                  )
+                ],
+              );
+            },
+            language: 'java',
+          ),
+          ulConfig: UlConfig(
+              dotMargin: const EdgeInsets.all(10),
+              crossAxisAlignment: CrossAxisAlignment.center),
+          olConfig: OlConfig(
+              olWrapper: ((Widget child) {
+                return Padding(
+                  padding: const EdgeInsets.only(top: 4, bottom: 4),
+                  child: child,
+                );
+              }),
+              crossAxisAlignment: CrossAxisAlignment.center),
+          codeConfig: CodeConfig(
+            codeStyle: TextStyle(color: Colors.green[700]),
+          ),
+          blockQuoteConfig: BlockQuoteConfig(
+            textConfig: TextConfig(),
+            blockStyle: const TextStyle(height: 1.5),
+          ),
+          titleConfig: TitleConfig(
+            showDivider: true,
+          ),
+        ),
+      );
+
+  Future<void> _launchUrl(Uri url) async {
+    if (!await launchUrl(url)) {
+      throw 'Could not launch $url';
+    }
   }
 }
